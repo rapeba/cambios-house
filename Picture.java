3f8d3fd906aaaed13259ffcb7e043b0382e49dc0
unknown <git config --global user.email>
2015-10-08 17:17:15 +0200

    cambios

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Circle sun2;
 
     /**
      * Constructor for objects of class Picture
@@ -49,11 +50,17 @@ public class Picture
         roof.makeVisible();
 
         sun = new Circle();
-        sun.changeColor("yellow");
+        sun.changeColor("blue");
         sun.moveHorizontal(100);
         sun.moveVertical(-40);
         sun.changeSize(80);
         sun.makeVisible();
+        
+        sun2 = new Circle();
+        sun2.makeVisible();
+        sun2.moveHorizontal(50);
+        sun2.changeColor("green");
+        
     }
 
     /**
